d2ac6a96318a27968d7e13a4de000034760d85dd
Mariana Soares <mariana.dsantos@al.infnet.edu.br>
2020-03-10 09:21:35 -0300

    criação de do projeto de infra da solução

---
+++ Infraestrutura/LivroRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Infraestrutura
+{
+    public class LivroRepository
+    {
+        //private List<Livro> livroLista = new List<Livro>();
+    }
+}
